ce8852c5fe4195217f7805fd6fef3b63b9f06031
Matheus Sena <MathSena07@hotmail.com>
2022-09-19 21:44:36 -0300

    Criando primeiro projeto - Sorteador de times

---
+++ android/app/src/main/kotlin/com/mathsena/primeiro_projeto/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mathsena.primeiro_projeto
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.green,
+      ),
+      home: const MyHomePage(title: 'Quem será o próximo campeão?'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+  int _numeroAleatorio = 0;
+  List _times = [
+    'Corinthians',
+    'Palmeiras',
+    'Santos',
+    'São Paulo'
+  ];
+
+  void _incrementCounter() {
+    setState(() {
+      
+      _numeroAleatorio = new Random().nextInt(4);
+
+      
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Pressione o botão para sortear um campeão: ',
+            ),
+            Text(
+              _times[_numeroAleatorio],
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
